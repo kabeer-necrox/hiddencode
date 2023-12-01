1e53c131cda64dbf42d0f17a8655ccbb3fe93b18
kabeer-necrox <khushkabeer@gmail.com>
2023-12-01 22:37:36 +0300

    here is the code

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // console.log("this is working")
 
+// const color = ["red", "blue", "green","yellow"]
+// const items = color.map(color => `<li>{color}<li>`)
+// console.log(color)
 
 
 
-const color = ["red", "blue", "green","yellow"]
 
-const items = color.map(color => `<li>{color}<li>`)
-console.log(color)
+const a = [2, 1, 4, 3];
+console.log(a);
+const b= a.sort();
+console.log(b);
+console.log(a)
+
+
+
+
+
+
+
+
+
