554d4d79340332c99728e564c0d1c1410b97ae58
kabeer-necrox <khushkabeer@gmail.com>
2023-12-02 03:15:35 +0300

    here is hte code

--- app.js
+++ app.js
@@ -21,24 +21,40 @@
 
 
 
-function asynchronous_operational_method() {
-    let first_promise = 
-        new Promise((resolve, reject) => resolve("Hello"));
-    let second_promise = 
-        new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(" GeeksforGeeks..");
-        }, 1000);
-    });
-    let combined_promise = 
-        Promise.all([first_promise, second_promise]);
-    return combined_promise;
+// function asynchronous_operational_method() {
+//     let first_promise = 
+//         new Promise((resolve, reject) => resolve("Hello"));
+//     let second_promise = 
+//         new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(" GeeksforGeeks..");
+//         }, 1000);
+//     });
+//     let combined_promise = 
+//         Promise.all([first_promise, second_promise]);
+//     return combined_promise;
+// }
+// async function display() {
+//     let data = await asynchronous_operational_method();
+//     console.log(data);
+// }
+// display();
+
+
+
+// if (navigator.onLine) {
+//     console.log('User is online');
+//   } else {
+//     console.log('User is offline');
+//   }
+let a = 5;
+function square(number){
+    return number * 6;
 }
-async function display() {
-    let data = await asynchronous_operational_method();
-    console.log(data);
-}
-display();
+let ans = square(a)
+console.log(ans)
+
+
 
 
 
